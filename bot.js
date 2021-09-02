6759e10733d9ccf341fae07276b8ac3f58eb81ca
Benjamin Hollon <benjaminbhollon@gmail.com>
2021-09-02 09:53:59 +0800

    Get challenge IDs by title

--- bot.js
+++ bot.js
@@ -1,79 +1,89 @@
-// Require the habitica-boss package from npm
-const {Bot, ChallengeSet} = require('habitica-boss');
+(async () => {
+  // Require the habitica-boss package from npm
+  const {Bot, ChallengeSet} = require('habitica-boss');
 
-// Initialize the dashboard variable
-let dashboard = null;
+  // Require the getChallengeIDs module
+  const getChallengeIDs = require('./modules/getChallengeIDs.js');
 
-//Get configuration
-const config = require('./config.json');
+  // Initialize the dashboard variable
+  let dashboard = null;
 
-// Create the list of challenges
-const challenges = new ChallengeSet(require('./challenges.json').list);
+  //Get configuration
+  const config = require('./config.json');
+  let challenges = require('./challenges.json').list;
 
-// Get authentication information
-let auth = require('./auth.json');
-auth.bot.platform = auth.owner.platform = auth.owner.id + '-' + config.bot.name;
+  // Get authentication information
+  let auth = require('./auth.json');
+  auth.bot.platform = auth.owner.platform = auth.owner.id + '-' + config.bot.name;
 
-// Create the bot
-const bot = new Bot(challenges, auth.bot);
+  // Get the challenge IDs
+  challenges = await getChallengeIDs(challenges, auth.owner);
+  console.log(challenges);
 
-// Schedule daily updates
-async function update() {
-  // Update the member stats
-  await bot.updateMembers().then(() => {
-    console.info('Member stats updated.');
-    // Calculate new stats based on the updated values
-    bot.updateStats();
-  });
-}
+  // Create the list of challenges
+  challenges = new ChallengeSet(challenges);
 
-//Send a message with the compiled report in your guild
-async function sendReport() {
-  console.log('Sending a report to your guild...');
-  bot.sendReport(config.guild.id, config.report.append);
-}
+  // Create the bot
+  const bot = new Bot(challenges, auth.bot);
 
-// Set up dashboard
-function setUpDashboard() {
-  // Templates
-  dashboard.templates('pug', './dashboard/templates/');
+  // Schedule daily updates
+  async function update() {
+    // Update the member stats
+    await bot.updateMembers().then(() => {
+      console.info('Member stats updated.');
+      // Calculate new stats based on the updated values
+      bot.updateStats();
+    });
+  }
 
-  // Static content
-  dashboard.static('./dashboard/static/');
-
-  dashboard.get('/', async (request, response) => {
-    response.render('homepage.pug', {challenges});
-  });
-
-  dashboard.get('/challenge/:id', async (request, response) => {
-    const challenge = challenges.list.find(c => c.id === request.params.id);
-
-    if (typeof challenge === 'undefined') response.status(404);
-
-    response.render('challenge.pug', {challenge});
-  });
-}
-
-// Run an update now to get some initial stats
-console.info('Running an initial update...');
-update().then(() => {
-  // If you set in config to send the update onStart, send it
-  if (config.report.onStart) sendReport();
-
-  // Start the dashboard
-  dashboard = require('./dashboard/app.js');
+  //Send a message with the compiled report in your guild
+  async function sendReport() {
+    console.log('Sending a report to your guild...');
+    bot.sendReport(config.guild.id, config.report.append);
+  }
 
   // Set up dashboard
-  setUpDashboard();
-});
+  function setUpDashboard() {
+    // Templates
+    dashboard.templates('pug', './dashboard/templates/');
 
-// Schedule a daily update that sends to your guild at midnight each day
-bot.scheduleTask(() => {
-  console.log('Updating stats...');
-  update().then(sendReport);
-});
+    // Static content
+    dashboard.static('./dashboard/static/');
 
-// Do an extra update every three hours for the dashboard
-bot.scheduleTask(() => {
-  update();
-}, `0 3,6,9,12,15,18,21 * * *`);
+    dashboard.get('/', async (request, response) => {
+      response.render('homepage.pug', {challenges});
+    });
+
+    dashboard.get('/challenge/:id', async (request, response) => {
+      const challenge = challenges.list.find(c => c.id === request.params.id);
+
+      if (typeof challenge === 'undefined') response.status(404);
+
+      response.render('challenge.pug', {challenge});
+    });
+  }
+
+  // Run an update now to get some initial stats
+  console.info('Running an initial update...');
+  update().then(() => {
+    // If you set in config to send the update onStart, send it
+    if (config.report.onStart) sendReport();
+
+    // Start the dashboard
+    dashboard = require('./dashboard/app.js');
+
+    // Set up dashboard
+    setUpDashboard();
+  });
+
+  // Schedule a daily update that sends to your guild at midnight each day
+  bot.scheduleTask(() => {
+    console.log('Updating stats...');
+    update().then(sendReport);
+  });
+
+  // Do an extra update every three hours for the dashboard
+  bot.scheduleTask(() => {
+    update();
+  }, `0 3,6,9,12,15,18,21 * * *`);
+})();
